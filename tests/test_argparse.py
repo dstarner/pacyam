625a9ad9b37da05a724c475fdd8db9004c6ac45e
Daniel Starner <dstarner@bloomberg.net>
2018-11-07 11:32:41 -0500

    Removed all f strings

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -15,28 +15,24 @@ class CLITestCase(unittest.TestCase):
     project_root = os.path.join(REPO_ROOT, 'tests', 'project')
 
     def test_directory(self):
-        arguments = f'{self.project_root}'
+        arguments = self.project_root
         options = parse_arg_helper(arguments)
         self.assertEqual(options.directory, self.project_root)
 
     def test_config(self):
         config = 'hello'
 
-        arguments = f'{self.project_root} --config {config}'
+        arguments = '%s --config %s' % (self.project_root, config)
         options = parse_arg_helper(arguments)
         self.assertEqual(options.config_path, config)
 
-        arguments = f'{self.project_root} -c {config}'
+        arguments = '%s -c %s' % (self.project_root, config)
         options = parse_arg_helper(arguments)
         self.assertEqual(options.config_path, config)
 
     def test_load_global_variables(self):
         variables = 'hello=world'
 
-        arguments = f'{self.project_root} -v {variables}'
-        options = parse_arg_helper(arguments)
-        self.assertEqual(options.vars, [variables])
-
-        arguments = f'{self.project_root} -v {variables}'
+        arguments = '%s -v %s' % (self.project_root, variables)
         options = parse_arg_helper(arguments)
         self.assertEqual(options.vars, [variables])
